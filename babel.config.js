361d57cb8e1d02d240b75f5a8c2ad0f25abf26b3
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-08-05 11:48:16 +0900

    chore: 프로젝트 초기설정

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // 최신 JavaScript 기능을 사용하기 위한 프리셋
+    "@babel/preset-react", // React용 프리셋
+    "@babel/preset-typescript", // TypeScript용 프리셋 (TypeScript를 사용하는 경우)
+  ],
+  plugins: [
+    [
+      "babel-plugin-module-resolver",
+      {
+        root: ["./src"], // 소스 코드의 루트 디렉토리 설정
+        alias: {
+          "~": "./src", // ~를 src 디렉토리로 매핑
+        },
+      },
+    ],
+    [
+      "dotenv-import",
+      {
+        moduleName: "@env", // 환경 변수 모듈 이름
+        path: ".env", // 환경 변수 파일 경로
+        safe: false, // 안전 모드 사용 여부
+        allowUndefined: true, // 정의되지 않은 변수를 허용할지 여부
+      },
+    ],
+  ],
+};
