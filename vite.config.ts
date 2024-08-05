713f5adc3fc7f0655dcfce8cbd6f25175fef4fa4
umuno <yoomoonho1997@gmail.com>
2024-08-05 21:26:09 +0900

    changing vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-//
-const path = require("path");
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const path = require('path');
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
   server: {
     port: 3000,
