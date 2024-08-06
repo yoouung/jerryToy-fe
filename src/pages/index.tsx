c63926162c7528aff7b74b72acb3dd837c26a09f
yoouung <jyp101400@gmail.com>
2024-08-06 14:33:37 +0900

    Style: container 스타일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import DrawerComponent from '../components/drawer';
-import { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -7,47 +6,12 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const MapContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const DrawerWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  z-index: 1000;
-`;
-
-declare global {
-  interface Window {
-    kakao: any;
-  }
-}
-
-const Map = () => {
-  const [map, setMap] = useState(null);
-  const { kakao } = window;
-
-  useEffect(() => {
-    const mapContainer = document.getElementById('map'); // 지도를 표시할 div
-    const mapOption = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      level: 10, // 지도의 확대 레벨
-    };
-
-    setMap(new kakao.maps.Map(mapContainer, mapOption));
-  }, []);
-
-  // TODO: drawer-list 위치 수정
-
+const OutLetContainer = () => {
   return (
     <Container>
-      <MapContainer id="map" />
-      <DrawerWrapper>
-        <DrawerComponent />
-      </DrawerWrapper>
+      <Outlet />
     </Container>
   );
 };
 
-export default Map;
+export default OutLetContainer;
