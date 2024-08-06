710aba26985c4a514e9b9fe03d76fa73c7f5e374
yoouung <jyp101400@gmail.com>
2024-08-06 11:10:28 +0900

    Style: GlobalStyles 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './styles/globalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
 );
